ce108e27043cd48bb1fe6b9ba1d2c83bcc93bd88
Bool Kev <boolkevin2@gmail.com>
2025-02-23 10:29:38 +0000

    Impl du design de quelques composants
    	-design Login
    	-List Blog
    	-Show Blog
    	-Starting design form
    	-creation de layout pour le navbar et le footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { HomeLayoutComponent } from './components/layout/home-layout/home-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { ListingComponent } from './components/blog/listing/listing.component';
+import { ShowComponent } from './components/blog/show/show.component';
+import { FormComponent } from './components/blog/form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        redirectTo: '/acceuil',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: HomeLayoutComponent,
+        children: [
+            {
+                path: 'acceuil',
+                component: ListingComponent
+            },
+            {
+              path: 'article/nouveau',
+              component: FormComponent
+            },
+            {
+                path: 'article/:slug-:id',
+                component: ShowComponent
+            }
+        ]
+    }
+];
---
+++ src/app/components/layout/home-layout/home-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home-layout',
+  imports: [RouterOutlet],
+  templateUrl: './home-layout.component.html',
+  styleUrl: './home-layout.component.css'
+})
+export class HomeLayoutComponent {
+
+}
