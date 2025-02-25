eb7605edf31f24cb8d4ebe1cf718071780d28b76
Bool Kev <boolkevin2@gmail.com>
2025-02-25 12:53:17 +0000

    MAJ du Readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  contenta: ["./src/**/*.{js,ts}"],
+  content: ["./src/**/*.{js,ts}"],
   darkMode: 'class',
   theme: {
     extend: {
